69b4d81040dddac52aa93a11e5c800dae4c966be
Bibek <bibekbhujel077.com>
2021-11-24 19:59:42 +0545

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:make_catalog/screens/login_page.dart';
 
-import 'home_page.dart';
+import 'screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+//home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(primarySwatch: Colors.red),
+
+      initialRoute: "/home",
+
+      routes: {"/": (context) => LoginPage(), "/home": (context) => HomePage()},
+    );
+  }
 }
