cc226ab23dede076adff81f86af327abd47324da
Bibek <bibekbhujel077.com>
2021-11-24 18:54:08 +0545

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Catalog App'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('welcome to flutter '),
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
