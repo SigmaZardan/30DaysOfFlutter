49f771d60e0eb48c64aea6ea27489ab917f2a696
Bibek <bibekbhujel077.com>
2021-11-25 17:01:36 +0545

    day 4 completed

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-        child: Center(
-      child: Text(
-        'Bibek Bhujel',
-        style: TextStyle(
-            fontSize: 20.0, color: Colors.blue, fontWeight: FontWeight.bold),
-        textScaleFactor: 2.0,
-      ),
-    ));
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/login_image.png',
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: GoogleFonts.lato().fontFamily,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: Column(children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Enter username', labelText: 'Username'),
+                ),
+                TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter password', labelText: 'Pass')),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.black54),
+                    onPressed: () {},
+                    child: Text('Login'))
+              ]),
+            )
+          ],
+        ));
   }
 }
